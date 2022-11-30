5366d6d3b424ab77f2bf0638a1d45cac02c31c78
Gediminas Eglinskas <gediminas@Gediminass-MacBook-Air.local>
2022-12-01 00:05:56 +0200

    dbsql replace

--- backend/accounts_202211302357.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO vigi36task.accounts (group_id,user_id) VALUES
-	 (3,1),
-	 (2,1);
--- backend/bills_202211302357.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO vigi36task.bills (group_id,amount,description) VALUES
-	 (1,20.0,'Uz registracijos bilieta'),
-	 (2,15.0,'Uz produktus');
